117c7ebcb22fd096cb021459ac182361ffb2226b
AashikMadhavarao <56664369+AashikMadhavarao@users.noreply.github.com>
2022-08-24 18:46:15 +0530

    Create UploadBlobFile.ps1

---
+++ azure-sqlvm-migration-service/UploadBlobFile.ps1
@@ -0,0 +1,16 @@
+param(
+    [string]
+    $Resourcegroupname
+)
+
+if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
+	{
+       Install-Module -Name Az.Accounts -Force
+    }
+
+$storageaccount = Get-AzStorageAccount -ResourceGroupName $Resourcegroupname;
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $storageaccount.StorageAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $storageaccount.StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Set-AzStorageBlobContent -File $($pwd)\AdventureWorksLT2019.bak -Container "backup" -Blob "AdventureWorksLT2019" -Context $ctx
