1937e8d8513d0351031be5ec1923a6be28bc598d
AashikMadhavarao <56664369+AashikMadhavarao@users.noreply.github.com>
2022-08-26 13:06:32 +0530

    Update UploadBlobFile.ps1

--- azure-sqlvm-migration-service/UploadBlobFile.ps1
+++ azure-sqlvm-migration-service/UploadBlobFile.ps1
@@ -13,9 +13,9 @@ if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
 
 $bacpacFileName = "AdventureWorksLT2019.bak";
 $storageaccount = Get-AzStorageAccount -ResourceGroupName $Resourcegroupname;
-$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $storageaccount.StorageAccountName;
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $storageaccount.StorageAccountName[1];
 
-$ctx = New-AzStorageContext -StorageAccountName $storageaccount.StorageAccountName -StorageAccountKey $storageaccountkey.Value[0]
+$ctx = New-AzStorageContext -StorageAccountName $storageaccount.StorageAccountName[1] -StorageAccountKey $storageaccountkey.Value[0]
 
 Invoke-WebRequest -Uri $uri -OutFile '$bacpacFileName'
 Set-AzStorageBlobContent -File $bacpacFileName -Container "backup" -Blob "AdventureWorksLT2019" -Context $ctx
