ca31698c7fe5a5338be6c85346285b17bbc2a6ac
AashikMadhavarao <56664369+AashikMadhavarao@users.noreply.github.com>
2022-08-26 12:27:53 +0530

    Update UploadBlobFile.ps1

--- azure-sqlvm-migration-service/UploadBlobFile.ps1
+++ azure-sqlvm-migration-service/UploadBlobFile.ps1
@@ -16,4 +16,5 @@ $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupna
 
 $ctx = New-AzStorageContext -StorageAccountName $storageaccount.StorageAccountName[1] -StorageAccountKey $storageaccountkey.Value[0]
 
-Invoke-WebRequest -Uri $uri -OutFile AdventureWorksLT2019.bak; Set-AzStorageBlobContent -File 'AdventureWorksLT2019.bak' -Container "backup" -Blob "AdventureWorksLT2019" -Context $ctx
+Invoke-WebRequest -Uri $uri -OutFile '$HOME/AdventureWorksLT2019.bak'
+Set-AzStorageBlobContent -File '$HOME/AdventureWorksLT2019.bak' -Container "backup" -Blob "AdventureWorksLT2019" -Context $ctx
