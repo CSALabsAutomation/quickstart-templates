7a67008cf6f8113f72707e4b56a484574ae34cd2
AashikMadhavarao <56664369+AashikMadhavarao@users.noreply.github.com>
2022-08-26 15:40:27 +0530

    Update UploadBlobFile.ps1

--- azure-sqlvm-migration-service/UploadBlobFile.ps1
+++ azure-sqlvm-migration-service/UploadBlobFile.ps1
@@ -11,11 +11,11 @@ if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
        Install-Module -Name Az.Accounts -Force
     }
 
-$bacpacFileName = "AdventureWorksLT2019.bak";
+$bacpacFileName = "AdventureWorks.bacpac";
 $storageaccount = Get-AzStorageAccount -ResourceGroupName $Resourcegroupname;
 $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $storageaccount.StorageAccountName[1];
 
 $ctx = New-AzStorageContext -StorageAccountName $storageaccount.StorageAccountName[1] -StorageAccountKey $storageaccountkey.Value[0]
 
-Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
-Set-AzStorageBlobContent -File $bacpacFileName -Container "backup" -Blob $bacpacFileName -Context $ctx
+Invoke-WebRequest -Uri 'https://github.com/CSALabsAutomation/quickstart-templates/blob/main/azure-sqlvm-migration-service/AdventureWorks.bacpac' -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "backup" -Blob 'AdventureWorksLT2019' -Context $ctx
