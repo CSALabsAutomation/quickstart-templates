0a206d4fc4b5de11ae298f889c90769088368f61
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-10-06 13:29:14 +0530

    Update uploadfile.ps1

--- azure-synapse/Sample/uploadfile.ps1
+++ azure-synapse/Sample/uploadfile.ps1
@@ -1,1 +1,12 @@
 Write-Host "file upload script called"
+$Resourcegroupname = az-lab-purview;
+$RawDataLakeAccountName = pvlabebe1ceadls;
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-synapse-labs/main/environments/env1/Sample/Artifacts/TaxiDataFiles/Geography.csv";
+$bacpacFileName = "Geography.csv";
+#$storageaccount = Get-AzStorageAccount -ResourceGroupName $Resourcegroupname;
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $RawDataLakeAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $RawDataLakeAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName 
+Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'Geography.csv' -Context $ctx
