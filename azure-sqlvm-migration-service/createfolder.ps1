d8d38b0a1678b4fd263ca319452a1d4521d07a69
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2023-03-02 13:47:22 +0530

    Update createfolder.ps1

--- azure-sqlvm-migration-service/createfolder.ps1
+++ azure-sqlvm-migration-service/createfolder.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest 'https://raw.githubusercontent.com/CSALabsAutomation/quickstart-templates/main/azure-sqlvm-migration-service/createfolder.ps1' -OutFile C:/Users/sqladmin/createfolder.ps1
+Invoke-WebRequest 'https://raw.githubusercontent.com/CSALabsAutomation/quickstart-templates/main/azure-sqlvm-migration-service/createfolder.ps1' -OutFile C:\Users\sqladmin\createfolder.ps1
